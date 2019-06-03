01a7da9d9686ceab026df2c2eff656bf3122500d
nicolascessieux <nicolascessieux@hotmail.fr>
2019-06-03 17:18:13 +0200

    HomePage Router et Form

--- upandwin/src/App.js
+++ upandwin/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home';
+import Store from './Redux/Store';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
-      home
+      <Switch>
+        <Provider store={Store}>
+          <Route exact path="/" component={Home} />
+          <Route path="/form/" component={Form} />
+        </Provider>
+      </Switch>
     </div>
   );
 }
