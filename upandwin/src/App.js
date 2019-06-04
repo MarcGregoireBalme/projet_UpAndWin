d610b72ab81d589932ea32fb70c12cfe9b251f14
yann <yann.mongoin@gmail.com>
2019-06-04 10:10:47 +0200

    modal

--- upandwin/src/App.js
+++ upandwin/src/App.js
@@ -8,6 +8,7 @@ import Lol from './pages/Lol';
 import Wow from './pages/Wow';
 import Search from './pages/Search';
 import Profil from './pages/Profil';
+import ModalSuccess from './components/ModalSuccess';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/Wow" component={Wow} />
           <Route path="/Search" component={Search} />
           <Route path="/Profil" component={Profil} />
+          <ModalSuccess />
         </Provider>
       </Switch>
     </div>
---
+++ upandwin/src/components/ModalSuccess.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+
+class ModalSuccess extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      show: true,
+    };
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    const { show } = this.state;
+    return (
+      <div>
+        <Modal id="modalAlerte" show={show} onHide={this.handleClose}>
+          <Modal.Header id="sasa" closeButton>
+            <Modal.Title><p id="victory">Sucess !</p></Modal.Title>
+          </Modal.Header>
+          <Modal.Body id="modalBody">
+            <div>
+              coucou
+            </div>
+          </Modal.Body>
+          <Modal.Footer id="modalFoot">
+            <NavLink to="./">
+              <Button className="buttonModalScore" onClick={this.handleClose}>
+              Retour
+              </Button>
+            </NavLink>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalSuccess;
