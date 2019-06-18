80281a0170a92f10c45c6995f6c8b2e094d0424f
Marc-Gregoire Balme <marcbalme@yahoo.com>
2019-06-18 15:33:28 +0200

    searchbar

--- upandwin/src/Components/SearchBar.jsx
+++ upandwin/src/Components/SearchBar.jsx
@@ -1,36 +1,69 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import './SearchBar.css';
+import axios from 'axios';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import './SearchBar.css';
-
+import RatingStar from './StarRating';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       searchtext: '',
+      videos: [],
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  componentDidMount() {
+    axios.get('http://localhost:3005/videos')
+      .then((res) => {
+        this.setState({ videos: res.data });
+      });
+  }
+
+  handleChange(event) {
+    this.setState({ searchtext: event.target.value });
   }
 
   render() {
-    const { videos } = this.props;
-    console.log(videos);
+    const { videos, searchtext } = this.state;
+
     return (
-      <Paper className="WinRoot">
-        <InputBase
-          className="WinInput"
-          placeholder="Search"
-          inputProps={{ 'aria-label': 'Search' }}
-        />
-        <IconButton className="WinIconButton" aria-label="Search">
-          <SearchIcon />
-        </IconButton>
-        <Divider className="WinDivider" />
-      </Paper>
+      <div>
+        <div>
+          <h1>
+            <Paper className="Winroot">
+              <InputBase
+                onChange={e => this.handleChange(e)}
+                className="Wininput"
+                placeholder="Rechercher une vidéo"
+                inputProps={{ 'aria-label': 'Search' }}
+              />
+            </Paper>
+          </h1>
+          <div className="container-fluid">
+            <div className="row videoDisplay">
+              {videos.filter(test => test.titre.toLowerCase().indexOf(searchtext.toLowerCase()) !== -1).map(video => (
+                <div key={video.titre} className="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
+                  <h3>{video.titre}</h3>
+                  <RatingStar />
+                  <iframe
+                    title={video.titre}
+                    width="100%"
+                    height="250px"
+                    src={video.lien}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
