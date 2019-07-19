f627eb533832741bb8fedb99def3992040ae46a3
Benoit Briastre <benoit.briastre@gmail.com>
2019-07-19 12:07:57 +0200

    ajout fonction show more qui améliore le chargement de la page Home & de la page LolVideos

--- upandwin/src/Components/DisplayVideo.jsx
+++ upandwin/src/Components/DisplayVideo.jsx
@@ -1,20 +1,49 @@
 /* eslint-disable no-underscore-dangle */
-import React from 'react';
+import React, { Component } from 'react';
 import './displayVideo.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Video from './Video';
 
-const DisplayVideo = ({ videos }) => (
-  <div className="container-fluid">
-    <div className="row videoDisplay">
-      {videos/* .filter((x, id) => id < 4) */.map(video => (
-        <div key={video._id} className="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
-          <Video video={video} videosFav={videos} />
+class DisplayVideos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      range: 10,
+    };
+    this.showMore = this.showMore.bind(this);
+  }
+
+  showMore() {
+    const { range } = this.state;
+    this.setState({
+      range: range + 5,
+    });
+  }
+
+  loopVideos() {
+    const { range } = this.state;
+    const { videos } = this.props;
+    for (let i = 0; i < range; i++) {
+      return (
+        videos.filter((video, id) => id < range).map(video => (
+          <div key={video._id} className="col-xl-3 col-lg-4 col-sm-6 col-xs-12" style={{ minWidth: '320px' }}>
+            <Video video={video} />
+          </div>
+        ))
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row videoDisplay">
+          {this.loopVideos()}
+          <button type="button" onClick={() => this.showMore()} className="Showmorebutton">Voir plus</button>
         </div>
-      ))}
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
-
-export default DisplayVideo;
+export default DisplayVideos;
